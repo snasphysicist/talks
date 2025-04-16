4323896ba46b956f640420f00063e22dd591d5ac
Scott Smith <snas@physicist.net>
2025-04-16 07:40:59 +0100

    fix: make pdf/html generation for dont be an iter hater work with docker

--- convert.sh
+++ convert.sh
@@ -1,5 +1,17 @@
 ROOT_DIR="/home/scottsmith/Projects/talks"
 
+docker run \
+  --mount type=bind,source=$ROOT_DIR,target=/mnt/talks/ \
+  marpteam/marp-cli /mnt/talks/dont-be-an-iter-hater/dont-be-an-iter-hater.md \
+  --allow-local-files \
+  -o /mnt/talks/dont-be-an-iter-hater/dont-be-an-iter-hater.html
+
+# docker run \
+#   --mount type=bind,source=$ROOT_DIR,target=/mnt/talks/ \
+#   marpteam/marp-cli /mnt/talks/dont-be-an-iter-hater/dont-be-an-iter-hater.md \
+#   --allow-local-files \
+#   --pdf -o /mnt/talks/dont-be-an-iter-hater/dont-be-an-iter-hater.pdf
+
 cd cognit-go
 npx @marp-team/marp-cli@latest $ROOT_DIR/cognit-go/cognit-go.md \
   --allow-local-files \
@@ -10,9 +22,22 @@ npx @marp-team/marp-cli@latest $ROOT_DIR/cognit-go/cognit-go.md \
   --pdf \
   -o $ROOT_DIR/cognit-go/cognit-go.pdf
 
-npx @marp-team/marp-cli@latest $ROOT_DIR/dont-be-an-iter-hater/dont-be-an-iter-hater.md \
+docker run \
+  -e MARP_USER="$(id -u):$(id -g)" \
+  --mount type=bind,source=$ROOT_DIR,target=/mnt/talks/ \
+  marpteam/marp-cli \
+  /mnt/talks/dont-be-an-iter-hater/dont-be-an-iter-hater.md \
   --allow-local-files \
-  -o $ROOT_DIR/dont-be-an-iter-hater/dont-be-an-iter-hater.html
+  -o /mnt/talks/dont-be-an-iter-hater/dont-be-an-iter-hater.html
+
+docker run \
+  -e MARP_USER="$(id -u):$(id -g)" \
+  --mount type=bind,source=$ROOT_DIR,target=/mnt/talks/ \
+  marpteam/marp-cli \
+  /mnt/talks/dont-be-an-iter-hater/dont-be-an-iter-hater.md \
+  --allow-local-files \
+  --pdf \
+  -o /mnt/talks/dont-be-an-iter-hater/dont-be-an-iter-hater.pdf
 
 npx @marp-team/marp-cli@latest $ROOT_DIR/dont-be-an-iter-hater/dont-be-an-iter-hater.md \
   --allow-local-files \
